4f473e06683c9605dd9cef99e3ca6806da125b3f
onkar ungratwar <ungratwaronkar@gmail.com>
2023-09-15 12:54:18 +0530

    files

--- src/routes/v1/student.session.route.js
+++ src/routes/v1/student.session.route.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const validate = require('../../middlewares/validate');
-const  StudentSessionValidation  = require('../../validations/student.session.validation');
+const StudentSessionValidation = require('../../validations/student.session.validation');
 const studentSessionController = require('../../controllers/student.session.controller');
-
 
 const router = express.Router();
 router
