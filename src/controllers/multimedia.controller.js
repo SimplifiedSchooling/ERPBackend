87f65c2b51adecab30d66f73b3568e52e4e9d964
suhas <suhas@simplifiedschooling.com>
2023-12-08 10:59:32 +0530

    changes

--- src/controllers/multimedia.controller.js
+++ src/controllers/multimedia.controller.js
@@ -6,12 +6,14 @@ const { multimediaService } = require('../services');
 const { filterPath } = require('../utils/s3middleware');
 
 const createMultimedia = catchAsync(async (req, res) => {
+
   if (req.files[0] && req.files[0].location) {
     req.body.icon1 = await filterPath(req.files[0].location);
   }
   if (req.files[1] && req.files[1].location) {
     req.body.icon2 = await filterPath(req.files[1].location);
   }
+
   const multimedia = await multimediaService.createMultimedia(req.body);
   res.status(httpStatus.CREATED).send(multimedia);
 });
